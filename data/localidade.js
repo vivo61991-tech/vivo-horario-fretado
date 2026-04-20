717a3449d61d7df15be24b8663749e1c81ef8678
vivo61991-tech <fabios.silva.a0061991@gmail.com>
2026-04-20 16:38:30 -0300

    tabela de endereço e coordenadas

---
+++ data/localidade.js
@@ -0,0 +1,27 @@
+const locais = [
+  {
+    nome: "BORBA GATO",
+    endereco: "Rua do Estilo Barroco, 318",
+    coordenadas: "-23.632914,-46.6934888",
+    latitude: -23.632914,
+    longitude: -46.6934888
+  },
+  {
+    nome: "GRANJA JULIETA",
+    endereco: "Rua Alexandre Dumas, 4403",
+    coordenadas: "-23.627820,-46.711355",
+    latitude: -23.627820,
+    longitude: -46.711355
+  },
+  {
+    nome: "TELEFONICA",
+    endereco: "Rua Antônio das Chagas, 1196",
+    coordenadas: "-23.6291001,-46.7042669",
+    latitude: -23.6291001,
+    longitude: -46.7042669
+  }
+];
+
+// Exemplo de como acessar:
+console.log(locais[0].nome); // Saída: BORBA GATO
+console.log(locais[0].latitude); // Saída: -23.632914

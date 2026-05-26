657d8143eae093b8408109cb8a64a12b6a3e9e50
vivo61991-tech <fabios.silva.a0061991@gmail.com>
2026-05-25 22:26:29 -0300

    Update localidade.js
    Alteração de coordenadas latitude/longitude para padrão do Street View do Google

--- data/localidade.js
+++ data/localidade.js
@@ -2,26 +2,17 @@ const locais = [
   {
     nome: "BORBA GATO",
     endereco: "Rua do Estilo Barroco, 318",
-    coordenadas: "-23.632914,-46.6934888",
-    latitude: -23.632914,
-    longitude: -46.6934888
+    coordenadas: "@-23.63299,-46.6935492,3a,75y,73.98h,90.6t"
   },
   {
     nome: "MORUMBI",
     endereco: "Av. das Nações Unidas, 14261",
-    coordenadas: "-23.6231229,-46.7026637",
-    latitude: -23.6285233,
-    longitude: -46.7101018
+    coordenadas: "@-23.6220347,-46.7014569,3a,75y,74.2h,94.12t"
   },
   {
     nome: "TELEFONICA",
     endereco: "Rua Antônio das Chagas, 1196",
-    coordenadas: "-23.628909, -46.704325",
-    latitude: -23.628909,
-    longitude: -46.704325
+    coordenadas: "@-23.6289517,-46.7045004,3a,75y,92.98h,83.58t"
   }
 ];
 
-// Exemplo de como acessar:
-console.log(locais[0].nome); // Saída: BORBA GATO
-console.log(locais[0].latitude); // Saída: -23.632914
